25e195022d674be453c5278f57b8bbe012107b0f
yoii18 <dpssalhotra@gmail.com>
2026-06-01 21:30:35 +0530

    container shell command

--- scripts/lib/modules.sh
+++ scripts/lib/modules.sh
@@ -10,6 +10,21 @@ create_resource_group(){
 create_storage_account(){
     local acct_name="$1" rg_name="$2"
     if ! az storage account show -n "$acct_name" -g "$rg_name" &>/dev/null; then
-        az storage account create 
+        az storage account create \
+            -n "$acct_name" \
+            -g "$rg_name" \
+            --min-tls-version "TLS1_2" \
+            --sku "Standard_LRS" \
+            --hns true
+    fi
+}
+
+create_container(){
+    local cont_name="$1" acct_name="$2"
+    if [ "$(az storage container exists -n "$cont_name" --account-name "$acct_name" --auth-mode login --query exists)" = "false" ]; then
+        az storage container create \
+            -n "$cont_name" \
+            --account-name "$acct_name" \
+            --auth-mode login
     fi
 }
