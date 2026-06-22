29063e04411cb0fc3446b992a34e19417ebb46ea
yoii18 <dpssalhotra@gmail.com>
2026-06-22 11:42:41 +0530

    pl test

--- modules/pl/main.tf
+++ modules/pl/main.tf
@@ -58,3 +58,44 @@ resource "azurerm_data_factory_custom_dataset" "adls_ds" {
   })
   schema_json = jsonencode([])
 }
+
+
+################ Pipeline ################
+
+resource "azurerm_data_factory_pipeline" "copy_data_pl" {
+  data_factory_id = var.adf_id
+  name            = "copy_data_pl"
+  activities_json = jsonencode([
+    {
+      name           = "Copy Data"
+      type           = "Copy"
+      dependsOn      = []
+      userProperties = []
+      typeProperties = {
+        source = {
+          type               = "RestSource"
+          httpRequestTimeout = "00:05:00"
+          requestMethod      = "GET"
+        }
+        sink = {
+          type = "AzureBlobFSSink"
+          storeSettings = {
+            type = "AzureBlobFSWriteSettings"
+          }
+          formatSettings = {
+            type = "JsonWriteSettings"
+          }
+        }
+        enableStaging = false
+      }
+      inputs = [{
+        referenceName = azurerm_data_factory_custom_dataset.rest_api_ds.name
+        type          = "DatasetReference"
+      }]
+      outputs = [{
+        referenceName = azurerm_data_factory_custom_dataset.adls_ds.name
+        type          = "DatasetReference"
+      }]
+    }
+  ])
+}
