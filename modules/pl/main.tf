37eb8ed165d391b8e5b99ee3ba9bbf6c584d88a1
yoii18 <dpssalhotra@gmail.com>
2026-06-22 11:20:17 +0530

    added datasets

--- modules/pl/main.tf
+++ modules/pl/main.tf
@@ -23,3 +23,38 @@ resource "azurerm_data_factory_linked_custom_service" "adf_adls_ls" {
   })
   data_factory_id = var.adf_id
 }
+
+################ Data Sets ################
+
+resource "azurerm_data_factory_custom_dataset" "rest_api_ds" {
+  name            = "rest_api_ds"
+  type            = "RestResource"
+  data_factory_id = var.adf_id
+  linked_service {
+    name = azurerm_data_factory_linked_custom_service.name.name
+  }
+  type_properties_json = jsonencode({
+    relativeUrl   = "users/yoii18"
+    requestMethod = "GET"
+    requestBody   = ""
+  })
+}
+
+resource "azurerm_data_factory_custom_dataset" "adls_ds" {
+  name            = "adls_ds"
+  type            = "Json"
+  data_factory_id = var.adf_id
+  linked_service {
+    name = azurerm_data_factory_linked_custom_service.adf_adls_ls.name
+  }
+  type_properties_json = jsonencode({
+    location = {
+      type       = "AzureBlobFSLocation"
+      fileSystem = "staging"
+      folderPath = "raw/data"
+      fileName   = "data.json"
+    }
+    encodingName = "UTF-8"
+  })
+  schema_json = jsonencode([])
+}
